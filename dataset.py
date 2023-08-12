9369326f6af5dd15760eacf06acc8e29f8c3a91b
luxian <luxianlebron@163.com>
2023-08-12 11:23:12 +0800

    update

--- dataset.py
+++ dataset.py
@@ -5,28 +5,34 @@ import torch.utils.data as data
 import torchvision.transforms as transforms
 
 import cv2
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
+VOC_CLASSES = (    # always index 0
+    'aeroplane', 'bicycle', 'bird', 'boat',
+    'bottle', 'bus', 'car', 'cat', 'chair',
+    'cow', 'diningtable', 'dog', 'horse',
+    'motorbike', 'person', 'pottedplant',
+    'sheep', 'sofa', 'train', 'tvmonitor')
+
+
+
 class YoloDataset(data.Dataset):
-    def __init__(self, root, list_file, train, transform) -> None:
+    def __init__(self, file_path, transform) -> None:
         super(YoloDataset, self).__init__()
-        self.root = root
-        self.train = train
+        self.file_path = file_path
         self.transform = transform
         self.fnames = []
         self.boxes = []
         self.labels = []
         self.mean = (123, 117, 104) # RGB
 
-        if isinstance(list_file, list):
-            tmp_file = './tmp/listfile.txt'
-            os.system('cat %s > %s' % (' '.join(list_file), tmp_file))
-            list_file = tmp_file
-        
-        with open(list_file) as f:
+        with open(file_path) as f:
             lines = f.readlines()
-        
+
         for line in lines:
             splited = line.strip().split()
+            self.fnames.append()
             
+
